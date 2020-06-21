f15172b751e41d50154e32029f27d591fa3d8831
Sima <sima.boreiri@gmail.com>
2020-06-21 14:11:38 +0200

    adds illustration to the portfolio using undraw library

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { UndrawDesignerLife } from "react-undraw-illustrations";
 
 const About = () => {
   return (
     <div className="ui main container">
-      <h1 id="about-header" className="ui header">About Me</h1>
-      <p>
-       Would you like to know me?
-      </p>
+      <div class="ui stackable two column grid">
+        <div class="column">
+          <UndrawDesignerLife primaryColor='#12283a'height='200px' />
+        </div>
+        <div class="column">
+          <h1 className="ui header">About Me</h1>
+          <p>
+          Would you like to know me?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
