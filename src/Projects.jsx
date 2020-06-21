19a03b414ff28f1651c43fe981e49ca7755c3a68
Sima <sima.boreiri@gmail.com>
2020-06-21 11:08:59 +0200

    creates test file for project list, modifies package json, addes codes to project file and adds componentDidMount() event

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from "axios";
 
-const Projects = () => {
+class Projects extends Component {
+  state = {
+    projects: []
+  };
+  render() {
+    const projects = this.state.projects;
+    let projectsList;
+
+    if (projects.length > 0) {
+      projectsList = projects.map(project => {
+        return (
+          <div id={'project-' + project.id} key={project.id}>
+            <h3 className="ui header">{project.name}</h3>
+          </div>
+        );
+      });
+    };
+
   return (
     <div className="ui main container">
       <h1 id="projects-header" className="ui header">My Projects</h1>
-      <p>
-       would you like to know about my projects?
-      </p>
+   {projectsList}
     </div>
   );
+}; 
+componentDidMount() {
+  axios.get('./src/data/projects.json')
+    .then(response => {
+      this.setState({
+        projects: response.data
+      });
+    });
 };
+}
 
 export default Projects;
