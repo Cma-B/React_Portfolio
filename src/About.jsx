133a2ac00695bae360d3717b3e89e1d2ce77f3e7
Sima <sima.boreiri@gmail.com>
2020-06-21 02:46:05 +0200

    provides basic navigation by adding routers and navlink to the code files and writes the navigation test and passes

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="ui main container">
+      <h1 id="about-header" className="ui header">About Me</h1>
+      <p>
+       Would you like to know me?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="ui main container">
+      <h1 id="projects-header" className="ui header">My Projects</h1>
+      <p>
+       would you like to know about my projects?
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
