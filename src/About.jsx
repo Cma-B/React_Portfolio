82090b42c459479a7ab997497985f6c11c2b6307
Sima <sima.boreiri@gmail.com>
2020-06-22 02:50:47 +0200

    adds about me text and edits my cv file and changes the images

--- src/About.jsx
+++ src/About.jsx
@@ -11,7 +11,11 @@ const About = () => {
         <div class="column">
           <h1 id="about-header" className="ui header">About Me</h1>
           <p>
-          Would you like to know me?
+          
+          Due to my enthusism in making new experiences, I moved to Sweden in 2018.
+          I was born in 1986 in Tehran, Iran and raised there.
+          I started to work as a technical sales manager for eight years after graduation.
+          But currently, I have started a new journey in Information Technology world.
           </p>
         </div>
       </div>
