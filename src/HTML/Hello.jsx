9c2efe2153018006e7b04875c7a9aaf223e2cc02
Sima <sima.boreiri@gmail.com>
2020-06-30 14:56:52 +0200

    last styling

--- src/HTML/Hello.jsx
+++ src/HTML/Hello.jsx
@@ -3,18 +3,20 @@ import React from "react";
 const Hello = () => {
   return (
     <div className="ui main container">
-      <div class="ui stackable two column grid">
-        <div class="column">
+      <div class="ui stackable two row grid">
+        <div class="row">
+         
+          </div>
          
         </div>
-        <div class="column">
+        <div class="row">
           <h1 id="hello" className="ui header">PORTFOLIO</h1>
           <p>
  SIMA BOREIRI
           </p>
         </div>
       </div>
-    </div>
+    
   );
 };
 
